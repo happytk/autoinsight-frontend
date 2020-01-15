3ffef38e7f3f825c899eff043d23e6668968b180
강정모(Kang Jeong Mo)/플랫폼_Tech1그룹/SK <kajmokr@sk.com>
2020-01-15 17:18:40 +0900

    [Deploy] jQuery로 전환중

--- autoinsight_frontend/static/autoinsight_frontend/js/deploy.js
+++ autoinsight_frontend/static/autoinsight_frontend/js/deploy.js
@@ -1,5 +1,11 @@
 var $FRONTEND = (function (module) {
     var _p = module._p = module._p || {};
+    var columns =[]
+    _p.init = function () {
+        _p.getStatus()
+        _p.getDeployment()
+        _p.getColumns()
+    }
 
     _p.getStatus = function(){
         return $.ajax({
@@ -9,19 +15,100 @@ var $FRONTEND = (function (module) {
             success: function (resultData, textStatus, request) {
                 if (resultData['error_msg'] == null ){
                     if(resultData['live']===true){
-                        $('#loader').addClass("loader");
+                        $('#leaderboard_loader').addClass("loader")
                     }else{
-                        $('#loader').removeClass("loader");
+                        $('#leaderboard_loader').removeClass("loader")
                     }
                 } else {
-                    console.log(resultData['error_msg']);
+                    console.log(resultData['error_msg'])
                 }
             },
             error: function (res) {
-                console.log("Ajax call failure");
+                console.log("Ajax call failure")
             }
         })
-    };
+    }
+
+    _p.getDeployment = function(){
+        return $.ajax({
+            type: 'get',
+            url: g_RESTAPI_HOST_BASE + 'deployment/',
+            dataType: 'json',
+            success: function (resultData, textStatus, request) {
+                if (resultData['error_msg'] == null ){
+                    $('#estimator_name').text(resultData.target.estimatorName)
+                    $('#score').text(resultData.target.score)
+                    $('#created_at').text(resultData.createdAt)
+                } else {
+                    console.log(resultData['error_msg'])
+                }
+            },
+            error: function (res) {
+                console.log("Ajax call failure")
+            }
+        })
+    }
+
+    _p.getColumns = function(){
+        return $.ajax({
+            type: 'get',
+            url: g_RESTAPI_HOST_BASE + 'dataset/columns/',
+            dataType: 'json',
+            success: function (resultData, textStatus, request) {
+                if (resultData['error_msg'] == null ){
+                    var tablehtml = ""
+                    $(resultData).each(function(index, column) {
+                        if(column.isTarget === false){
+                            columns.push(column.name)
+                            tablehtml += '<tr><td>'+column.name+'</td><td>'+column.datatype+'</td><td><input type="text" class="form-control predict-input" mean="'+column.mean+'"></td></tr>'
+                        }
+                    })
+                    $('#predict_table').html(tablehtml)
+                } else {
+                    console.log(resultData['error_msg'])
+                }
+            },
+            error: function (res) {
+                console.log("Ajax call failure")
+            }
+        })
+    }
+
+    _p.fillMean = function(){
+        $(".predict-input").each(function(index, value) {
+            $(this).val($(this).attr("mean"))
+        })
+    }
+
+    _p.predict = function(){
+        var data = {}
+        inputstring =""
+        len = $(".predict-input").length
+        $(".predict-input").each(function(index, value) {
+            inputstring+=$(this).val()
+            if(index+1 < len) inputstring+= ","
+        })
+        data.inputs = inputstring
+
+        return $.ajax({
+            type: 'post',
+            url: g_RESTAPI_HOST_BASE + 'deployment/predict/',
+            data: data,
+            dataType: 'json',
+            success: function (resultData, textStatus, request) {
+                if (resultData['error_msg'] == null ){
+                    alert("예측결과 : "+resultData.result[0])
+
+                } else {
+                    console.log(resultData['error_msg'])
+                }
+            },
+            error: function (res) {
+                console.log("Ajax call failure")
+            }
+        })
+    }
+
 
     return module;
 }($FRONTEND || {}));
