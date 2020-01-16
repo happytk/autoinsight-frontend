233018038009fc6f82222dd7a1bb1b215a1a2eeb
강태욱(KANG TAEWOOK)/플랫폼_Tech1그룹/SK <wecanfly@sk.com>
2020-01-16 15:31:07 +0900

    setup.py for dist.

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ setup(
     # For a discussion on single-sourcing the version across setup.py and the
     # project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='1.3.1',  # Required
+    version='1.3.1b',  # Required
 
     # This is a one-line description or tagline of what your project does. This
     # corresponds to the "Summary" metadata field:
@@ -118,7 +118,7 @@ setup(
 
     # When your source code is in a subdirectory under the project root, e.g.
     # `src/`, it is necessary to specify the `package_dir` argument.
-    package_dir={'': 'autoinsight_frontend'},  # Optional
+    package_dir={'': '.'},  # Optional
 
     # You can just specify package directories manually here if your project is
     # simple. Or you can use find_packages().
